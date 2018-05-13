c6792b786b21f1dcea85c45af3dbdd11c9a37f30
Ryosuke Miyamoto <yahsan2@gmail.com>
2018-05-14 00:15:07 +1000

    Updated

---
+++ pages/dream.vue
@@ -0,0 +1,41 @@
+<template>
+  <ArticleDetail
+    :article="article"
+    :postType="'dream'"
+  />
+</template>
+
+<script>
+import ArticleDetail from '~/components/ArticleDetail'
+
+export default {
+  async asyncData ({ app, store, params, route, payload }) {
+  },
+
+  data () {
+    return {
+      article: {
+        title: '助けてほしい夢',
+        excerpt: '誰かに手伝ってほしい夢や、成し遂げたいこと、仲間がほしいこと'
+      }
+
+    }
+  },
+
+  components: {
+    ArticleDetail
+  },
+
+  computed: {
+  },
+
+  head () {
+    return {
+      title: `${this.article.title} | ${this.$store.state.meta.name}`,
+      meta: [
+        { description: this.article.excerpt }
+      ]
+    }
+  }
+}
+</script>

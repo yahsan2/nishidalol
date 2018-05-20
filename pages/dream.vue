1e0a4b8f89ce6d5e71f77622fff7cef17ea38384
Ryosuke Miyamoto <yahsan2@gmail.com>
2018-05-20 15:11:22 +1000

    Fixed

--- pages/dream.vue
+++ pages/dream.vue
@@ -20,7 +20,7 @@ export default {
         excerpt: '誰かに手伝ってほしい夢や、成し遂げたいこと、仲間がほしいこと',
         images: [{
           'sizes': {
-            'large': {
+            'full': {
               'width': 1060,
               'height': 596,
               'source_url': 'https://s3-ap-northeast-1.amazonaws.com/nishidalol/wp/wp-content/uploads/2018/03/19194517/98C4C165-0F4C-430B-9EE6-2B38158F4B8A-e1526726717829.jpeg'
